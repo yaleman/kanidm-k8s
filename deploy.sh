346a6197ddc6b230a801f5d8622c1e88d739ad33
James Hodgkinson <james@terminaloutcomes.com>
2022-06-06 09:30:16 +1000

    updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Please specify a filename to deploy"
+    exit 1
+else
+    if [ ! -f "$1" ]; then
+        echo "Couldn't find {$1}, bailing!"
+        exit 1
+    fi
+fi
+
 #shellcheck disable=SC1091
 source .env
 
-envsubst < kanidmd.yaml | kubectl apply -f -
+envsubst < "$1" | kubectl apply -f -
